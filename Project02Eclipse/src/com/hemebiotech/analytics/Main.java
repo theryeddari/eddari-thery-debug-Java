d527e5dc88c572b6938f27c4ce9ceab939d6c459
theryeddari <121760376+theryeddari@users.noreply.github.com>
2023-10-14 14:39:27 +0200

    instantiation of objects from the three service classes

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,8 +1,16 @@
 package com.hemebiotech.analytics;
 
+import com.hemebiotech.analytics.interfaces.ISymptomReader;
+import com.hemebiotech.analytics.interfaces.ISymptomWriter;
+import com.hemebiotech.analytics.services.AnalyticsCounter;
+import com.hemebiotech.analytics.services.ReadSymptomDataFromFile;
+import com.hemebiotech.analytics.services.WriteSymptomDataToFile;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ISymptomReader symptomReader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+        ISymptomWriter symptomWriter = new WriteSymptomDataToFile();
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter(symptomReader, symptomWriter);
     }
 }
