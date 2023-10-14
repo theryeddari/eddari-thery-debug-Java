4fdb0dba021d5aff6af4e462c9dc4ba30a197ce0
theryeddari <121760376+theryeddari@users.noreply.github.com>
2023-10-14 14:45:37 +0200

    calls analyticsCounter methods in order

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -6,11 +6,19 @@ import com.hemebiotech.analytics.services.AnalyticsCounter;
 import com.hemebiotech.analytics.services.ReadSymptomDataFromFile;
 import com.hemebiotech.analytics.services.WriteSymptomDataToFile;
 
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
         ISymptomReader symptomReader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
         ISymptomWriter symptomWriter = new WriteSymptomDataToFile();
         AnalyticsCounter analyticsCounter = new AnalyticsCounter(symptomReader, symptomWriter);
+
+        List<String> symptoms = analyticsCounter.getSymptoms();
+        Map<String, Integer> symptomsCounted = analyticsCounter.countSymptoms(symptoms);
+        Map<String, Integer> symptomsSorted = analyticsCounter.sortSymptoms(symptomsCounted);
+        analyticsCounter.writeSymptoms(symptomsSorted);
     }
 }
