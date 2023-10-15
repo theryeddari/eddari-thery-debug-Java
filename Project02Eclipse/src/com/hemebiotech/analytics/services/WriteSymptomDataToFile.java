33c4735c2ec4341feb89546bade96f578f66c371
theryeddari <121760376+theryeddari@users.noreply.github.com>
2023-10-15 21:42:03 +0200

    add comments, javadoc, camelCase to WriteSymptomDataToFile.

--- Project02Eclipse/src/com/hemebiotech/analytics/services/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/services/WriteSymptomDataToFile.java
@@ -7,20 +7,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * A class that writes symptom data to a file.
+ */
 public class WriteSymptomDataToFile implements ISymptomWriter {
-    public void writeSymptoms(Map<String, Integer> symptoms){
 
-            //use try with resource in parentheses to automatically close.
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.out"))){
-                for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
-                    String symptom = entry.getKey();
-                    int number = entry.getValue();
-                    writer.write(symptom + ": " + number);
-                    writer.newLine();
-                }
-            } catch (IOException e) {
-                throw new IllegalStateException("Problème d'accès en écriture ou lecture");
+    /**
+     * Writes the symptoms and their counts to a file.
+     *
+     * @param symptoms The map of symptoms and their counts.
+     */
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.out"))) {
+            for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                String symptom = entry.getKey();
+                int number = entry.getValue();
+                writer.write(symptom + ": " + number);
+                writer.newLine();
             }
-            System.out.println("Le fichier a été correctement créé :)");
+        } catch (IOException e) {
+            throw new IllegalStateException("An error occurred while accessing or writing the file");
+        }
+        System.out.println("The file has been created successfully :)");
     }
 }
